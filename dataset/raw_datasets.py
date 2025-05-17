821b96444e428646886b180429f42f57a963e5ca
eamini97 <eamini.stuttgart@gmail.com>
2025-05-17 11:12:10 +0200

    Cleaned up tracked files with updated .gitignore

--- dataset/raw_datasets.py
+++ dataset/raw_datasets.py
@@ -7,16 +7,16 @@ from .dataset_config import SVHN_PATH, MNIST_PATH
 
 
 def get_svhn(train_transform, val_transform) -> tuple[datasets.SVHN, datasets.SVHN]:
-    train_ds = datasets.SVHN(SVHN_PATH, split="train",
+    train_ds = datasets.SVHN(SVHN_PATH, split="train", download=True,
                              transform=train_transform,)
-    val_ds = datasets.SVHN(SVHN_PATH, split="test", transform=val_transform)
+    val_ds = datasets.SVHN(SVHN_PATH, split="test", download=True, transform=val_transform)
     return train_ds, val_ds
 
 
 def get_mnist(train_transform, val_transform) -> tuple[datasets.MNIST, datasets.MNIST]:
-    train_ds = datasets.MNIST(MNIST_PATH, train=True,
+    train_ds = datasets.MNIST(MNIST_PATH, train=True, download=True,
                               transform=train_transform)
-    val_ds = datasets.MNIST(MNIST_PATH, train=False, transform=val_transform)
+    val_ds = datasets.MNIST(MNIST_PATH, train=False, download=True, transform=val_transform)
     return train_ds, val_ds
 
 
